6c74b60666fde4ecce399f7d0b3d76c33f36b264
NafeeJ <nafeejan2@gmail.com>
2021-09-27 15:48:07 -0400

    get top osu score from a user id form (untested)

--- main.py
+++ main.py
@@ -1,10 +1,51 @@
 import flask
+import requests
+from os import getenv
 
 app = flask.Flask(__name__)
+
+API_URL = 'https://osu.ppy.sh/api/v2'
+TOKEN_URL = 'https://osu.ppy.sh/oauth/token'
 
 @app.route('/')
 def root():
    return flask.redirect("/static/index.html", code=302)
+
+def get_token():
+   data = {
+      # TODO: use getenv() for id and secret later which needs to be set up in gcloud
+      'client_id': 10026,
+      'client_secret': 'EuPXSua2lkqyGuMKsnUJTSQYG2CLJhrTMoIcaA3z',
+      'grant_type': 'client_credentials',
+      'scope': 'public'
+   }
+
+   response = requests.post(TOKEN_URL, data=data)
+
+   return response.json().get('access_token')
+
+@app.route('/getscores')
+def get_scores():
+   user_id = flask.request.args['user_id_field']
+
+   token = get_token()
+
+   headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': f'Bearer {token}'
+   }
+
+   params = {
+      'mode': 'osu',
+      'limit': 1
+   }
+
+   response = requests.get(f'{API_URL}/users/{user_id}/scores/best', params=params, headers=headers)
+
+   user_scores = response.json()[0]
+
+   return flask.Response(user_scores, mimetype='text/html')
 
 
 if __name__ == '__main__':
